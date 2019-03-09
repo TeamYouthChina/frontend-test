f7cc11eee49259d06c3f429f46f598db0a275790
zzc-tongji <zzc-tongji@users.noreply.github.com>
2019-03-09 16:10:40 -0500

    commit

--- test.sh
+++ test.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-set -e
-set -u
-SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-echo pwd
-pwd
-echo
-echo ls
-ls
-echo
-if [[ ! -f pass.txt ]]; then
-    echo pass.txt not exist
-    exit 1
-else
-    echo pass.txt exist
-    exit 0
-fi
+exit 1
