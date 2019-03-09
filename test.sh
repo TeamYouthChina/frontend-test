e40da193a9bbb20e71a2851411539a958fc50070
zzc-tongji <zzc-tongji@users.noreply.github.com>
2019-03-09 15:04:04 -0500

    change test

--- test.sh
+++ test.sh
@@ -4,6 +4,16 @@ set -e
 set -u
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+echo pwd
+pwd
+echo
+echo ls
+ls
+echo
 if [[ ! -f pass.txt ]]; then
+    echo pass.txt not exist
     exit 1
+else
+    echo pass.txt exist
+    exit 0
 fi
